dc7158b3ff2b6989d1d9f8dbed81c8b25fb0a311
zulalrs <zulal.r.s@gmail.com>
2019-06-07 14:41:30 +0300

    arkaplan

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'blue',
   },
   welcome: {
     fontSize: 20,
